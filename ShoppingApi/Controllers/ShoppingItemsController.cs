4abdcd1530225341033272d453f09dac153d5379
Mike Jones <kiamel76@gmail.com>
2024-12-08 10:22:43 +0100

    Fixed sorting method in GET

--- ShoppingApi/Controllers/ShoppingItemsController.cs
+++ ShoppingApi/Controllers/ShoppingItemsController.cs
@@ -18,17 +18,22 @@ namespace ShoppingApi.Controllers
             _shoppingItemsCollection = shoppingItemsCollection;
         }
 
+        // GET: api/shoppingitems
         [HttpGet]
         public async Task<ActionResult<IEnumerable<ShoppingItem>>> GetShoppingItems()
         {
-            var items = await _shoppingItemsCollection.Find(item => true).ToListAsync();
+            var items = await _shoppingItemsCollection
+                .Find(item => true)
+                .SortBy(item => item.Position)
+                .ToListAsync();
             return Ok(items);
         }
 
+        // GET: api/shoppingitems/{id}
         [HttpGet("{id:length(24)}", Name = "GetShoppingItem")]
         public async Task<ActionResult<ShoppingItem>> GetShoppingItem(string id)
         {
-            var item = await _shoppingItemsCollection.Find<ShoppingItem>(item => item.Id == id).FirstOrDefaultAsync();
+            var item = await _shoppingItemsCollection.Find(item => item.Id == id).FirstOrDefaultAsync();
             if (item == null)
             {
                 return NotFound();
@@ -36,6 +41,7 @@ namespace ShoppingApi.Controllers
             return Ok(item);
         }
 
+        // POST: api/shoppingitems
         [HttpPost]
         public async Task<ActionResult<ShoppingItem>> CreateShoppingItem([FromBody] ShoppingItemDTO itemDto)
         {
@@ -55,6 +61,7 @@ namespace ShoppingApi.Controllers
             return CreatedAtRoute("GetShoppingItem", new { id = item.Id }, item);
         }
 
+        // PUT: api/shoppingitems/{id}
         [HttpPut("{id:length(24)}")]
         public async Task<IActionResult> UpdateShoppingItem(string id, [FromBody] ShoppingItemDTO itemDto)
         {
@@ -65,20 +72,23 @@ namespace ShoppingApi.Controllers
 
             var updatedItem = new ShoppingItem
             {
-                Id = id,
+                Id = id, // Use string ID directly if Id is a string
                 Name = itemDto.Name,
                 IsComplete = itemDto.IsComplete,
                 Position = itemDto.Position
             };
 
             var result = await _shoppingItemsCollection.ReplaceOneAsync(item => item.Id == id, updatedItem);
+
             if (result.MatchedCount == 0)
             {
                 return NotFound();
             }
-            return NoContent();
+
+            return Ok(updatedItem);
         }
 
+        // DELETE: api/shoppingitems/{id}
         [HttpDelete("{id:length(24)}")]
         public async Task<IActionResult> DeleteShoppingItem(string id)
         {
@@ -87,6 +97,33 @@ namespace ShoppingApi.Controllers
             {
                 return NotFound();
             }
+            return NoContent();
+        }
+
+        // PUT: api/shoppingitems/updateOrder
+        [HttpPut("updateOrder")]
+        public async Task<IActionResult> UpdateOrder([FromBody] List<ShoppingItem> reorderedItems)
+        {
+            if (reorderedItems == null || !reorderedItems.Any())
+            {
+                return BadRequest("Invalid item list.");
+            }
+
+            var bulkOps = new List<WriteModel<ShoppingItem>>();
+
+            foreach (var item in reorderedItems)
+            {
+                var filter = Builders<ShoppingItem>.Filter.Eq(x => x.Id, item.Id);
+                var update = Builders<ShoppingItem>.Update.Set(x => x.Position, item.Position);
+                var updateOne = new UpdateOneModel<ShoppingItem>(filter, update);
+                bulkOps.Add(updateOne);
+            }
+
+            if (bulkOps.Any())
+            {
+                await _shoppingItemsCollection.BulkWriteAsync(bulkOps);
+            }
+
             return NoContent();
         }
     }
